22a36f324857d4ce181fe079243be2edb5b250af
Don Park <donpark@snu.ac.kr>
2017-06-15 14:22:09 -0400

    Updated GOFperformance MAN page

--- R/GOFperformance.R
+++ R/GOFperformance.R
@@ -1,4 +1,6 @@
 #' Output Performance comparisons for simulated and observed hydrological time series data
+#' 
+#' Performes Nash-Sutcliffe Efficiency, relative Nash-Sutcliffe Efficiency, Root Mean Square Error, Relative Root Mean Square Error, Ratio of RMSE to the standard deviation of the observations, Percent Bias, and Kling-Gupta Efficiency
 #'
 #' @param sim numeric 'data.frame', 'matrix', or 'vector' with simulated values
 #' @param obs numeric 'data.frame', 'matrix', or 'vector' with observed values
