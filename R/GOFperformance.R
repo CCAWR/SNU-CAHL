7defb5833f13264f026b7ad20c3362042b968942
Don Park <donpark@snu.ac.kr>
2017-06-15 14:34:00 -0400

    Modified the performance indexes outputted

--- R/GOFperformance.R
+++ R/GOFperformance.R
@@ -1,6 +1,6 @@
 #' Output Performance comparisons for simulated and observed hydrological time series data
 #' 
-#' Performes Nash-Sutcliffe Efficiency, relative Nash-Sutcliffe Efficiency, Root Mean Square Error, Relative Root Mean Square Error, Ratio of RMSE to the standard deviation of the observations, Percent Bias, and Kling-Gupta Efficiency
+#' Performes Nash-Sutcliffe Efficiency, Root Mean Square Error, Relative Root Mean Square Error, Ratio of RMSE to the standard deviation of the observations, and Kling-Gupta Efficiency
 #'
 #' @param sim numeric 'data.frame', 'matrix', or 'vector' with simulated values
 #' @param obs numeric 'data.frame', 'matrix', or 'vector' with observed values
@@ -10,15 +10,13 @@ GOFperformance <- function(sim,obs) {
 
   # Collecting performance values
   nse <- NSE(sim, obs)
-  rnse <- rNSE(sim, obs)
   rmse <- rmse(sim, obs)
   rrmse <- rrmse(sim, obs)
   rsr <- rsr(sim, obs)
-  pbias <- pbias(sim, obs)
   kge <- KGE(sim, obs)
 
   # Formatting for output
-  GOFoutput <- data.frame(nse, rnse, rmse, rrmse, rsr, pbias, kge)
+  GOFoutput <- data.frame(nse, rmse, rrmse, rsr, kge)
 
   return(GOFoutput)
 
